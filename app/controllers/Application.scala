022c581b201cc9dc6d9f63847b6c1a3d1b004be7
petitviolet <violethero0820@gmail.com>
2014-06-05 15:13:09 +0900

    basic functions for todo app

---
+++ app/controllers/Application.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data.Forms._
+import play.api.data._
+import models.Task
+
+object Application extends Controller {
+
+  def test(message: String) = Action {
+    Ok(views.html.test(message))
+  }
+
+  def index = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+
+  }
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action{ implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
+
+  def deleteTask(id: Long) = Action {implicit request =>
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  }
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+}
---
+++ app/models/Task/Task.scala
@@ -0,0 +1,40 @@
+package models
+
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import play.api.Play.current
+/**
+ * Created by Komurasaki on 2014/06/03.
+ */
+case class Task(id: Long, label: String)
+
+object Task {
+  def all(): List[Task] = DB.withConnection { implicit c =>
+    SQL("SELECT * FROM task").as(task *)
+  }
+
+  def create(label: String) {
+    DB.withConnection {implicit c =>
+      SQL("INSERT INTO task (label) VALUES ({label})").on(
+        'label -> label
+      ).executeUpdate()
+    }
+  }
+
+  def delete(id: Long) {
+    DB.withConnection {implicit c =>
+      SQL("DELETE FROM task WHERE id = {id}").on(
+        'id -> id
+      ).executeUpdate()
+    }
+  }
+
+  val task = {
+    get[Long]("id") ~
+      get[String]("label") map {
+      case id~label => Task(id, label)
+    }
+  }
+
+}
